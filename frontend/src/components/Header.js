307e3967c1f305a4f691acda24e217059dc1c799
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2020-06-10 22:30:15 +0200

    added header from material design

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  span: {
+    fontSize: 15,
+    marginLeft: 10,
+  },
+}));
 
 export const Header = () => {
+  const classes = useStyles();
   return (
     <div>
-      <h1>File Uploader</h1>
-      <h3>Upload your file below:</h3>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            File Uploader ---
+            <span className={classes.span}>Upload your file below:</span>
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
